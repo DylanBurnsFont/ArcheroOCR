a6343934596a4f6df83069f633748244e1e5af4d
DylanBurnsFont <dylan.in.celra@gmail.com>
2026-01-11 13:40:45 +0100

    Removed files from tracking

--- extractScores.ps1
+++
@@ -1,4 +0,0 @@
-Set-ExecutionPolicy -Scope CurrentUser -ExecutionPolicy RemoteSigned
-python monsterInvasion.py --path "C:\Path\To\Images"
-# --path is a required argument specifying path.
-# --debug optional flag that shows you how the script is processing the images
