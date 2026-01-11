e81412b53f181f2e8c920454f73c0d70f32cd6d5
DylanBurnsFont <dylan.in.celra@gmail.com>
2026-01-11 13:46:24 +0100

    Udated .gitignore

---
+++ extractScores.ps1
@@ -0,0 +1,4 @@
+Set-ExecutionPolicy -Scope CurrentUser -ExecutionPolicy RemoteSigned
+python monsterInvasion.py --path C:\Users\dylan\ArcheroOCR\Images\Friday
+# --path is a required argument specifying path.
+# --debug optional flag that shows you how the script is processing the images
---
+++ extractScores.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+python monsterInvasion.py --path ArcheroOCR/Images/Friday
